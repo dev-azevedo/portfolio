36ffe540e44d73abcc2df5aa5679d9027d680b1e
Jhonatan Azevedo <dev.azevedo@outlook.com>
2024-12-02 16:28:31 -0300

    CHANGE: title componente 'me'

--- src/components/me/Main.vue
+++ src/components/me/Main.vue
@@ -1,10 +1,10 @@
 <template>
   <section
     id="home"
-    class="mt-20 flex flex-col xl:flex-row justify-center lg:justify-start lg:p-0 gap-3 items-center p-5 bg-emerald-400 rounded-md w-full"
+    class="mt-20 flex flex-col xl:flex-row justify-center lg:justify-start lg:p-0 items-center bg-emerald-400 rounded-md w-full"
   >
     <div
-      class="h-96 bg-slate-300 flex justify-center items-center rounded-s-md"
+      class="h-96 2 bg-slate-300 flex justify-center items-center rounded-s-md"
     >
       <img
         src="@/assets/me.jpg"
@@ -12,7 +12,7 @@
         class="h-full rounded-s-md bg-gradient-to-tr from-emerald-400 to-slate-300"
       />
     </div>
-    <div class="text-white">
+    <div class="text-white  w-3/4">
       <h1 class="font-bold text-6xl 2xl:text-8xl">Jhonatan Azevedo</h1>
       <h3 class="text-sm text-gray-600">Desenvolvedor de software</h3>
     </div>
